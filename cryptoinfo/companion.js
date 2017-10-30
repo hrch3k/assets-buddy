b1b1d4332bc580806970b5cb6fc1926157220b47
duleonni <duleml@gmail.com>
2017-10-30 22:25:45 +0100

    Cracking eggs

--- cryptoinfo/companion.js
+++ cryptoinfo/companion.js
@@ -1,28 +1,71 @@
 $(document).ready(function(){
-    coin = "Ethereum"
-    function getCoininfo(coin_name){
+    var coin = "Ethereum"
+    var wallet = ""
+    var etherchain = "https://etherchain.org/api/account/" + wallet
+    var coinmarketcap = "https://api.coinmarketcap.com/v1/ticker/" + coin + "/?convert=EUR"
+    
+
+
+
+    function ajaxwallet(url, wallet){
         $.ajax({
-            url: "https://api.coinmarketcap.com/v1/ticker/" + coin_name + "/?convert=EUR",
+            url: url + wallet,
             method: "get",
-            data: {q: coin },
             dataType: "json",
             success: function(data){
-                parsed = JSON.stringify(data)
-
-                console.log(data[0])     
+                if (data.length == 0){
+                    parsed = JSON.stringify(data)
+                    console.log("This is Wallet info")
+                    console.log(data)
+                }else{
+                    $("#wallet").val("")
+                    $("#wallet").attr("placeholder", "Invalid address!");
+                    console.log("ivalid address") 
+                }     
             }
         })
     }
-    
-   $(".getaddres").on("click", function(){
-        var wallet = $("#wallet").val()
+
+
+    function ajaxcoin(){
+        $.ajax({
+            url: coinmarketcap,
+            method: "get",
+            dataType: "json",
+            success: function(data){
+                parsed = JSON.stringify(data)
+                console.log("This is coin info")
+                console.log(data)     
+            }
+        })
+    }
+
+    function searchWallet(){
+            wallet = $("#wallet").val()
         if (/^0x[a-fA-F0-9]{40}$/.test(wallet)){
-            console.log(wallet)
+            //console.log()
             $("#wallet").val("")
             $("#wallet").attr("placeholder", "Enter your Wallet address");
+            ajaxwallet(etherchain, wallet)
+
         }else{
+            $("#wallet").val("")
+            $("#wallet").attr("placeholder", "Invalid address!");
             console.log("ivalid address")
         }
 
+
+    }
+
+
+
+    $(".getcoin").on("click", function(){  
+        ajaxcoin()
+    })
+
+    
+   $(".getaddres").on("click", function(){
+        searchWallet()
+       
     })
 })
